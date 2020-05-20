85f119387bd97b23712ab83c920d5a27a7a2cd04
Anuneet Anand <anuneetanand@gmail.com>
2020-05-20 16:12:14 +0530

    Corrected Bugs, Please complete commented out Optimisation
    180s : N = 200

--- 2018022_endsem.py
+++ 2018022_endsem.py
@@ -2,11 +2,37 @@
 # Rhythm Patel [2018083]
 # EndSem Programming Assignment
 
+import time
 from collections import deque
+
+starttime=time.time()
 
 def Check(n,r,c):
 	if r>=0 and r<n and c>=0 and c<n:
 		return True
+	else:
+		return False
+
+# def DFS(s,t,G,C,F,P,V,Vis):
+# 	print(Vis)
+# 	if s in Vis:
+# 		if Vis[s]==1 and s!="S":
+# 			P ={}
+# 			return True
+# 	else:
+# 		Vis[s]=1
+# 		Flag = False
+# 		V[s]=1
+# 		if t in G[s]:
+# 			P[t]=s
+# 			return True
+# 		for x in G[s]:
+# 			if x not in V and C[(s,x)]-F[(s,x)]>0:
+# 				P[x]=s
+# 				Flag = DFS(x,t,G,C,F,P,V,Vis)
+# 				if Flag:
+# 					break
+# 		return Flag
 
 def DFS(s,t,G,C,F):
 	Exist = False
@@ -16,7 +42,6 @@ def DFS(s,t,G,C,F):
 	for x in G:
 		V[x]=0
 	Stack.append(s)
-	P[s]="Start"
 	while len(Stack) and not Exist:
 		x = Stack.pop()
 		V[x]=1
@@ -35,7 +60,7 @@ def DFS(s,t,G,C,F):
 A = []
 B = []
 
-with open("input.txt","r") as InputFile:
+with open("input-1.txt","r") as InputFile:
 	I = InputFile.readlines()
 	n = int(I[0])
 	Grid = [[0 for i in range(n)] for j in range(n)]
@@ -48,19 +73,21 @@ with open("input.txt","r") as InputFile:
 				else:
 					B.append((r,c))
 
-if (len(A)!=len(B)) or ((len(A)+len(B))%2!=0):
+if (len(A)!=len(B)):
 	print(0)
 else:
 	Graph = {}
 	Capacity = {}
 
+	for a in A:
+		Graph[a]=[]
+	for b in B:
+		Graph[b]=[]
+
 	for i in range(len(A)):
 		r,c = A[i][0],A[i][1]
-		Graph[(r,c)]=[]
-
 		if Check(n,r+1,c):
 			if Grid[r+1][c]==1:
-				Graph[r+1,c] = []
 				Graph[r,c].append((r+1,c))
 				Capacity[((r,c),(r+1,c))]=1
 				Graph[r+1,c].append((r,c))
@@ -68,7 +95,6 @@ else:
 
 		if Check(n,r,c+1):
 			if Grid[r][c+1]==1:
-				Graph[r,c+1]=[]
 				Graph[r,c].append((r,c+1))
 				Capacity[((r,c),(r,c+1))]=1
 				Graph[r,c+1].append((r,c))
@@ -76,7 +102,6 @@ else:
 
 		if Check(n,r-1,c):
 			if Grid[r-1][c]==1:
-				Graph[r-1,c]=[]
 				Graph[r,c].append((r-1,c))
 				Capacity[((r,c),(r-1,c))]=1
 				Graph[r-1,c].append((r,c))
@@ -84,7 +109,6 @@ else:
 
 		if Check(n,r,c-1):
 			if Grid[r][c-1]==1:
-				Graph[r,c-1]=[]
 				Graph[r,c].append((r,c-1))
 				Capacity[((r,c),(r,c-1))]=1
 				Graph[r,c-1].append((r,c))
@@ -93,21 +117,20 @@ else:
 	Flow = {}
 	Graph["S"]=[]
 	Graph["T"]=[]
-
+	Vis={}
 	for x in A:
 		Graph["S"].append(x)
 		Capacity[("S",x)] = 1
-		Capacity[(x,"S")] = 0
+		Vis[x]=0
 	for y in B:
 		Graph[y].append("T")
 		Capacity[(y,"T")] = 1
-		Capacity[("T",y)] = 0
 	for z in Capacity:
 		Flow[z]=0
-
 	MaxFlow = 0
+
 	Parent = DFS("S","T",Graph,Capacity,Flow)
-	while Parent!=0:
+	while Parent:
 		x = "T"
 		f =  1000000
 		while x!="S":
@@ -116,36 +139,29 @@ else:
 		MaxFlow += f
 		x = "T"
 		while x!="S":
-			Flow[(x,Parent[x])] -= f
 			Flow[(Parent[x],x)] += f
+			if x!="T" and Parent[x]!="S":
+				Flow[(x,Parent[x])] -= f
 			x = Parent[x]
+
 		Parent = DFS("S","T",Graph,Capacity,Flow)
 		#print(MaxFlow)
 
-	noOfPaths = 0
-
+	Answer = []
 	for x in Flow:
 		if Flow[x]>0 and "S" not in x and "T" not in x :
-			noOfPaths += 1
+			a,b = list(x[0]),list(x[1])
+			a[0]+=1
+			a[1]+=1
+			b[0]+=1
+			b[1]+=1
+			Answer.append([tuple(a),tuple(b)])
 
-	if (noOfPaths != len(A)):
+	if len(Answer)!=len(A):
 		print(0)
-
 	else:
-
 		print(1)
-		for x in Flow:
-			if Flow[x]>0 and "S" not in x and "T" not in x :
-				a,b = list(x[0]),list(x[1])
-				a[0]+=1
-				a[1]+=1
-				b[0]+=1
-				b[1]+=1
-				# print(tuple(a),tuple(b),sep="")
-				print( "(",a[0],",",a[1],")","(",b[0],",",b[1],")",sep="")
-
-
-
-
-
-
+		for i in Answer:
+			print(*i)
+endtime=time.time()
+print(endtime-starttime)
